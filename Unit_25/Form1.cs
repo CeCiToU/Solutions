8a79a5cd49a3e224d109b736e8db3ea68954b1cf
Tsvetoslav <cveti3060@gmail.com>
2022-03-17 19:55:32 +0200

    Unit 27

---
+++ Unit_25/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            int sum = 0;
+            if (number < 1000)
+            {
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                label1.Text = sum.ToString();
+            }
+            else
+            {
+                label1.Text = "Insert new number (-infinity; 1000)";
+            }
+        }
+    }
+}
---
+++ Unit_26/Problem_1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int numbersOfRods = 0;
+            if (b > a)
+            {
+                label6.Text = "a should be greater than b!";
+            }
+            else
+            {
+                while (a > b)
+                {
+                    numbersOfRods += 1;
+                    a = a - b;
+                }
+                if (a < b && a > 0 || a == b)
+                {
+                    numbersOfRods += 1;
+                }
+                label4.Text = numbersOfRods.ToString();
+            }
+           
+
+        }
+    }
+}
