58b693f1ac331df0e4b8574a3b60ceca2822a9c2
Tsvetoslav <cveti3060@gmail.com>
2022-04-01 23:48:03 +0300

    Random number generator

---
+++ Random_Solution/Problem_3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp39
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            label1.Text = random.Next(1, 6).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+    }
+}
