1739e4cb7583ecdbf04e303b5635895b4401f90e
Tsvetoslav <cveti3060@gmail.com>
2022-05-21 00:22:45 +0300

    Unit 34 and 35

---
+++ Unit_34/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ConsoleApp279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+Задача 3 
+
+ A) 
+
+ for (int i = 10; i <= 100; i += 10) 
+
+             { 
+
+                 Console.WriteLine(i); 
+
+             } 
+
+ Б) 
+
+ for (int i = 99; i >= 19; i -= 20) 
+
+             { 
+
+                 Console.WriteLine(i); 
+
+             } 
+
+ В) 
+
+ for (char i = 'A'; i <= 'Z'; i ++) 
+
+             { 
+
+                 Console.WriteLine(i); 
+
+             } 
+
+ Г) 
+
+ int num = 3; 
+
+             for (int i = 5; i < 43; i += 2) 
+
+             { 
+
+                 Console.WriteLine(num); 
+
+                 num += i; 
+
+             } 
+ Д) 
+
+ for (char i = 'a'; i < 'z'; i ++) 
+
+             { 
+
+                 Console.WriteLine(i); 
+
+             } 
+
+
+
+
+
+ Задача 4 
+
+ double c, f; 
+
+             Console.WriteLine("C        F"); 
+
+             for (c = 36.0; c < 38.0; c = c + 0.1) 
+
+             { 
+
+                 f = (9.0 / 5.0) * c + 32.0; 
+
+                 Console.Write($"{c:F2}    "); 
+
+                 Console.Write($"{f:F2}\n"); 
+
+             } 
+
+
+
+ Задача 5 
+
+ int n = int.Parse(Console.ReadLine()); 
+
+             int min = int.MaxValue; 
+
+             for(int i = 0; i < n; i++) 
+
+             { 
+
+                 int currentNumber = int.Parse(Console.ReadLine()); 
+
+                 if(currentNumber < min) 
+
+                 { 
+
+                     min = currentNumber; 
+
+                 } 
+
+             } 
+
+             Console.WriteLine(min); 
+
+
+
+ Задача 6 
+
+ int n = int.Parse(Console.ReadLine()); 
+
+             int sum = 0; 
+
+             for(int i = 0; i < n; i++) 
+
+             { 
+
+                 int currentNumber = int.Parse(Console.ReadLine()); 
+
+                 sum += currentNumber; 
+
+             } 
+
+             Console.WriteLine(sum); 
+             */
+        }
+    }
+}
---
+++ Unit_35/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp46
+{
+    public partial class Form1 : Form
+    {
+        int minNum = int.MaxValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            listBox1.Items.Clear();
+            Random r = new Random();
+            minNum = int.MaxValue;
+            for (int i = 0; i < 10; i++)
+            {
+                int numberRandom = r.Next() % 100 + 1;
+                listBox1.Items.Add(numberRandom);
+                if (minNum > numberRandom)
+                {
+                    minNum = numberRandom;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = minNum.ToString();
+        }
+    }
+}
